b8ebcea5ca9dbcd4d0a02abdce0741b69fe63901
Savannah Fox <114793890+savfox11@users.noreply.github.com>
2023-01-16 17:15:30 +1100

    Add files via upload

---
+++ pySumTwoNum.py
@@ -0,0 +1,8 @@
+#challenge 2: Sum of two numbers 
+#python
+fInput = input("Enter first number:")
+first = int(fInput)
+sInput = input("Enter second number:")
+second = int(sInput)
+result = first + second
+print("Result: ", result)
